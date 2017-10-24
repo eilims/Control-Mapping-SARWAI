928e3cc1d6988f5911699beb5218f6ed280f99f0
eilims <dbenusovich@gmail.com>
2017-10-24 11:57:08 -0700

    Adding in tf message modification, incomplete

--- husky_slam_simulation/src/HuskySlam.cpp
+++ husky_slam_simulation/src/HuskySlam.cpp
@@ -11,13 +11,27 @@ HuskySlam::HuskySlam()
 {
     this->sec = 0;
     this->nsec = 0;
+    this->tf.transforms.resize(1);
+    //Header assignments
+    this->tf.transforms[0].header.seq = 0;
+    this->tf.transforms[0].header.stamp.sec = this->sec;
+    this->tf.transforms[0].header.stamp.nsec = this->nsec;
+    this->tf.transforms[0].header.frame_id = "odom";
+    this->tf.transforms[0].child_frame_id = "base_link";
+    this->tf.transforms[0].transform.translation.x = 0;
+    this->tf.transforms[0].transform.translation.y = 0;
+    this->tf.transforms[0].transform.translation.z = 0;
+    this->tf.transforms[0].transform.rotation.x = 0;
+    this->tf.transforms[0].transform.rotation.y = 0;
+    this->tf.transforms[0].transform.rotation.z = 0;
+    this->tf.transforms[0].transform.rotation.w = 1;
 }
 
 HuskySlam::~HuskySlam()
 {
 }
 
-//We must republish odometry to the correct topic /odom, which google cartographer uses
+//We must republish odometry to the correct topic /tf, which google cartographer uses
 void HuskySlam::run()
 {
     ros::NodeHandle nh;
@@ -30,7 +44,7 @@ void HuskySlam::run()
 
     while (ros::ok())
     {
-        
+        parseTF();
         tfPub.publish(this->tf);
         ros::spinOnce();
         rate.sleep();
@@ -41,14 +55,23 @@ void HuskySlam::run()
 //We have to parse throughthe times to ensure the transform is valid
 void HuskySlam::updateTF(const tf::tfMessage& msg)
 {
+    if ((msg.transforms[0].header.stamp.sec >= this->sec) && (msg.transforms[0].header.stamp.nsec >= this->nsec))
+    {
+        this->sec = msg.transforms[0].header.stamp.sec;
+        this->nsec = msg.transforms[0].header.stamp.nsec;
         this->tf = msg;
+        
+    }
 }
 
 void HuskySlam::parseTF()
 {
     //We have to go through each transform and change base_link to test_link
-    for(int i = 0; i < sizeof(this->tf)/sizeof(this->tf[0]); i++)
+    for(int i = 0; i < sizeof(this->tf)/sizeof(this->tf.transforms[0]); i++)
     {
-
+        if (this->tf.transforms[i].child_frame_id == "base_link")
+        {
+            this->tf.transforms[i].child_frame_id = "test_link";
+        }
     }
 }
