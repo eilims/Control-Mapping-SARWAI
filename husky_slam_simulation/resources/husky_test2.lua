d91d1c09ed47aaa22313c7c1fcc0e5865e9e4757
eilims <dbenusovich@gmail.com>
2017-10-31 19:30:39 -0700

    modified for optimization (incomplete_

--- husky_slam_simulation/resources/husky_test2.lua
+++ husky_slam_simulation/resources/husky_test2.lua
@@ -28,12 +28,12 @@ options = {
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 10,
   num_point_clouds = 0,
-  lookup_transform_timeout_sec = 0.2,
+  lookup_transform_timeout_sec = 0.002,
   submap_publish_period_sec = 0.3,
-  pose_publish_period_sec = 5e-3,
-  trajectory_publish_period_sec = 30e-3,
+  pose_publish_period_sec = 5e-1,
+  trajectory_publish_period_sec = 30e-1,
   rangefinder_sampling_ratio = 1.,
-  odometry_sampling_ratio = 1.,
+  odometry_sampling_ratio = 0.5,
   imu_sampling_ratio = 1.,
 }
 
